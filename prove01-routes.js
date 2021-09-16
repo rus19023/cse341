474c84f5437d647b9fd6e4154f4286edc0060940
Doris Rush-Lopez <rus19023@byui.edu>
2021-09-16 03:19:48 -0400

    prove01-5

--- prove01-routes.js
+++ prove01-routes.js
@@ -3,7 +3,7 @@ const requestHandler = (req, res) => {
   const method = req.method;
   // show welcome message on root page
   if (url === '/') {
-    res.setHeader('Content-Type','text/html')
+    //res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>Welcome to our server!</title><head>');
     res.write('<body> <h1>Thanks for visiting our Node.js server!</h1> </body>');
@@ -13,24 +13,26 @@ const requestHandler = (req, res) => {
   }
   // show user list if in /users route
   if (url === '/users') {
-    res.setHeader('Content-Type', 'text/html');
+    //res.setHeader('Content-Type', 'text/html');
     res.write('<html><head><title>User List</title></head><body><section><ul><li>user1</li><li>user2</li></ul></section></body></html>');
   }
-  // create form to collect new username
+  // process form data collected in / page
   if (url === '/create-user' && method === 'POST') {
     const body = [];
     req.on('data', chunk => {
-      console.log(chunk);
+      //console.log(chunk);
       body.push(chunk);
-    return res.end();
+      return res.end();
     });
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const user = parsedBody.split('=')[1];
-        console.log(user)
-        return res.end();
+      console.log(user);
       });
     };
+    res.statusCode = 302;
+    //res.setHeader('Location', '/');
+    res.end();
   };
 
 exports.handler = requestHandler;
