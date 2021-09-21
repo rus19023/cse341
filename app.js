fd66faea33319971b8e52f3709c6dbabf33a31ab
Doris Rush-Lopez <rus19023@byui.edu>
2021-09-21 09:06:33 -0400

    fixed port and host

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3000'
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
